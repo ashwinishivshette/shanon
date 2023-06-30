2fba6694364a710b1acbbf2ee86b5a3fe83cd009
ashwinishivshette <138205725+ashwinishivshette@users.noreply.github.com>
2023-06-30 15:03:15 +0530

    Update fact.java

--- src/logical_june/fact.java
+++ src/logical_june/fact.java
@@ -3,6 +3,7 @@ package logical_june;
 public class fact {
 public static void main(String[] args) {
 	int num=11;
+	System.set.out.println("i am updated from github ");
 	int fact=1;
 	for(int i=1;i<=num;i++) {
 		fact=fact*i;
